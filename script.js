2e483748b8f80b6b952f30bc8265b5d8688ca8f5
claycaleb <claycaleb@github.com>
2024-06-02 22:11:47 -0400

    okay no header component, that's silly

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// Header Component
-const headerContent = `
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="/styles.css">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Red+Hat+Display:ital@0;1&display=swap" rel="stylesheet">
-<title>Caleb Clay</title>`;
-const header = document.querySelector("head")
-header.innerHTML = headerContent;
+// Title Component
 const title = document.querySelector("head > title");
 const contentTitle = document.querySelector("#content-title");
 if (contentTitle !== null) {
