ebf56e9422508dd425b37a2eed498f68c8cd8d28
claycaleb <claycaleb@github.com>
2024-06-04 12:09:51 -0400

    added ck script to all pages

--- script.js
+++ script.js
@@ -14,6 +14,4 @@ header.innerHTML = headerContent;
 
 // Footer Component
 const footer = document.querySelector("body > footer");
-footer.innerHTML = `
-<script async data-uid="bb8409e70b" src="https://calebclay.ck.page/bb8409e70b/index.js"></script>
-<p>&copy; 2024 Caleb Clay. All rights reserved.</p>`;
+footer.innerHTML = `<p>&copy; 2024 Caleb Clay. All rights reserved.</p>`;
