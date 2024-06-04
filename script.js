569a1616f96a377346dfa9ca3536412496daabc7
claycaleb <claycaleb@github.com>
2024-06-04 11:54:29 -0400

    footer test

--- script.js
+++ script.js
@@ -14,5 +14,6 @@ header.innerHTML = headerContent;
 
 // Footer Component
 const footer = document.querySelector("body > footer");
-const footerContent = "<p>&copy; 2024 Caleb Clay. All rights reserved.";
-footer.innerHTML = footerContent;
+footer.innerHTML = `
+<script async data-uid="bb8409e70b" src="https://calebclay.ck.page/bb8409e70b/index.js"></script>
+<p>&copy; 2024 Caleb Clay. All rights reserved.</p>`;
